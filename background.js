261e910cea5ae1dace49f8d1ecce8202b42e354d
sinv207 <sinv207@gmail.com>
2023-04-01 19:57:19 +0700

    [UPDATE] - Change app name; validate url before open it

--- background.js
+++ background.js
@@ -1,26 +1,47 @@
-let proxyHost = 'medium.first-aid.tech'
+let proxyHost = 'proxy.goenlab.com'
+
+const is_debug = false
+
+function isValidHttpUrl(string) {
+    try {
+        const newUrl = new URL(string)
+        return newUrl.protocol === 'http:' || newUrl.protocol === 'https:'
+    } catch (err) {
+        return false
+    }
+}
 
 chrome.runtime.onInstalled.addListener(function () {
-    console.log('Wellcome to First Aid Proxer')
+    console.log('Wellcome to GoEN Proxy')
     chrome.contextMenus.create({
         id: 'firstAidProxer',
-        title: 'Open with Fisrt Aid Proxer',
+        title: 'GoEN Proxy: Open page in New Tab',
         contexts: ['page'],
     })
 })
 
-chrome.contextMenus.onClicked.addListener(async function (info, tab) {
-    // console.log({ info, tab })
-    if (tab.url) {
-        let url = new URL(tab.url)
-        console.log({ tab, url })
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     console.log({ tabId, changeInfo, tab })
+//     if (tab.url && changeInfo.status) {
+//         console.log({ tabId, changeInfo, tab })
+//     }
+// })
 
-        if (url.hostname != proxyHost) {
-            let newUrl = new URL('https://' + proxyHost + url.pathname)
-            newUrl.searchParams.set('origin', url.hostname)
-            // console.log(newUrl)
-            // chrome.tabs.update(tab.id, { url: your_new_url })
-            chrome.tabs.create({ url: newUrl.href })
+chrome.contextMenus.onClicked.addListener(async function (info, tab) {
+    try {
+        if (tab.url) {
+            let url = new URL(tab.url)
+            is_debug && console.log({ tab, url })
+
+            if (url.hostname != proxyHost && isValidHttpUrl(url.href)) {
+                let newUrl = new URL('https://' + proxyHost + url.pathname)
+                newUrl.searchParams.set('origin', url.hostname)
+                // console.log(newUrl)
+                // chrome.tabs.update(tab.id, { url: your_new_url })
+                chrome.tabs.create({ url: newUrl.href })
+            }
         }
+    } catch (error) {
+        is_debug && console.trace(error)
     }
 })
--- popup.js
+++ popup.js
@@ -1,22 +1,37 @@
-let proxyHost = 'medium.first-aid.tech'
+let proxyHost = 'proxy.goenlab.com'
+
+const is_debug = false
+
+function isValidHttpUrl(string) {
+    try {
+        const newUrl = new URL(string)
+        return newUrl.protocol === 'http:' || newUrl.protocol === 'https:'
+    } catch (err) {
+        return false
+    }
+}
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 firstAidProxer.addEventListener('click', async () => {
-    let queryOptions = { active: true, currentWindow: true }
-    let [tab] = await chrome.tabs.query(queryOptions)
+    try {
+        let queryOptions = { active: true, currentWindow: true }
+        let [tab] = await chrome.tabs.query(queryOptions)
 
-    // console.log({ tab })
-    if (tab.url) {
-        let url = new URL(tab.url)
+        // console.log({ tab })
+        if (tab.url) {
+            let url = new URL(tab.url)
 
-        console.log({ tab, url })
+            is_debug && console.log({ tab, url })
 
-        if (url.hostname != proxyHost) {
-            let newUrl = new URL('https://' + proxyHost + url.pathname)
-            newUrl.searchParams.set('origin', url.hostname)
-            // console.log(newUrl)
-            // chrome.tabs.update(tab.id, { url: your_new_url })
-            chrome.tabs.create({ url: newUrl.href })
+            if (url.hostname != proxyHost && isValidHttpUrl(url.href)) {
+                let newUrl = new URL('https://' + proxyHost + url.pathname)
+                newUrl.searchParams.set('origin', url.hostname)
+                // console.log(newUrl)
+                // chrome.tabs.update(tab.id, { url: your_new_url })
+                chrome.tabs.create({ url: newUrl.href })
+            }
         }
+    } catch (error) {
+        is_debug && console.trace(error)
     }
 })
